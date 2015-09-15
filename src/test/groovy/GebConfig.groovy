3f92d71ccda4280ee7ae422044ff90ad6bf5d296
Fabian Kretzer <fabian.kretzer@eddyson.de>
2015-09-15 14:48:34 +0200

    added testing via sauce labs

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -8,8 +8,8 @@ environments {
     def sauce_username = System.properties['SAUCE_USERNAME']
     def sauce_access_key = System.properties['SAUCE_ACCESS_KEY']
     def tunnel_id = System.properties['TRAVIS_JOB_NUMBER']
-    baseUrl = "http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub"
-    def capa = DesiredCapabilities.chrome()
+    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
+    DesiredCapabilities capa = DesiredCapabilities.chrome()
     capa['tunnel-identifier'] = tunnel_id
     driver = {
       new RemoteWebDriver(baseUrl,capa)
