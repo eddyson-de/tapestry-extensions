3d0025b3ce5c11093fd434fd07f8269b8bc34d9a
Fabian Kretzer <fabian.kretzer@eddyson.de>
2015-09-15 17:01:27 +0200

    test in multiple browsers

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -3,17 +3,55 @@ import org.openqa.selenium.remote.RemoteWebDriver
 
 reportsDir = 'build/reports/geb'
 baseUrl = "http://localhost:${System.properties['jettyPort']}/"
+
+def sauce_username = System.properties['SAUCE_USERNAME']
+def sauce_access_key = System.properties['SAUCE_ACCESS_KEY']
+def tunnel_id = System.properties['TRAVIS_JOB_NUMBER']
 environments {
-  'travis' {
-    def sauce_username = System.properties['SAUCE_USERNAME']
-    def sauce_access_key = System.properties['SAUCE_ACCESS_KEY']
-    def tunnel_id = System.properties['TRAVIS_JOB_NUMBER']
+  'sauce_chrome' {
     URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
     DesiredCapabilities capa = DesiredCapabilities.chrome()
     capa.setCapability("tunnel-identifier", tunnel_id)
-x
+
     driver = {
       new RemoteWebDriver(baseUrl,capa)
     }
   }
+  'sauce_ie' {
+    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
+    DesiredCapabilities capa = DesiredCapabilities.internetExplorer()
+    capa.setCapability("tunnel-identifier", tunnel_id)
+
+    driver = {
+      new RemoteWebDriver(baseUrl,capa)
+    }
+  }
+  'sauce_firefox' {
+    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
+    DesiredCapabilities capa = DesiredCapabilities.firefox()
+    capa.setCapability("tunnel-identifier", tunnel_id)
+
+    driver = {
+      new RemoteWebDriver(baseUrl,capa)
+    }
+  }
+  'sauce_ipad' {
+    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
+    DesiredCapabilities capa = DesiredCapabilities.ipad()
+    capa.setCapability("tunnel-identifier", tunnel_id)
+
+    driver = {
+      new RemoteWebDriver(baseUrl,capa)
+    }
+  }
+  'sauce_safari' {
+    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
+    DesiredCapabilities capa = DesiredCapabilities.safari()
+    capa.setCapability("tunnel-identifier", tunnel_id)
+
+    driver = {
+      new RemoteWebDriver(baseUrl,capa)
+    }
+  }
+
 }
