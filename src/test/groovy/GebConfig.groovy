218902029281cddae5e400e594ca275a077a15aa
Yannick Dylla <ydylla@gmail.com>
2021-07-21 11:38:17 +0200

    build: use node gradle plugin to compile coffee script
    * use java 11

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -1,89 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.remote.DesiredCapabilities
-import org.openqa.selenium.remote.RemoteWebDriver
 
-import io.github.bonigarcia.wdm.ChromeDriverManager
-
-reportsDir = 'build/reports/geb'
+reportsDir = "build/reports/geb"
 baseUrl = "http://localhost:${System.properties['jettyPort']}/"
 
-def sauce_username = System.properties['SAUCE_USERNAME']
-def sauce_access_key = System.properties['SAUCE_ACCESS_KEY']
-def tunnel_id = System.properties['TRAVIS_JOB_NUMBER']
-
-driver = {
-  DesiredCapabilities.firefox().with {
-    setCapability("marionette", false);
-    new FirefoxDriver(it)
-  }
-}
-
 environments {
-  'sauce_chrome' {
-    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
-    DesiredCapabilities capa = DesiredCapabilities.chrome()
-    capa.setVersion("latest")
-    capa.setCapability("tunnel-identifier", tunnel_id)
-
-    driver = {
-      new RemoteWebDriver(baseUrl,capa)
-    }
-  }
-  'sauce_ie' {
-    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
-    DesiredCapabilities capa = DesiredCapabilities.internetExplorer()
-    capa.setCapability("platform", "Windows 7");
-    capa.setCapability("version", "11.0");
-    capa.setCapability("tunnel-identifier", tunnel_id)
-
-    driver = {
-      new RemoteWebDriver(baseUrl,capa)
-    }
-  }
-  'sauce_firefox' {
-    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
-    DesiredCapabilities capa = DesiredCapabilities.firefox()
-    capa.setVersion("latest")
-    capa.setCapability("tunnel-identifier", tunnel_id)
-
-    driver = {
-      new RemoteWebDriver(baseUrl,capa)
-    }
-  }
-  'sauce_ipad' {
-    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
-    DesiredCapabilities capa = DesiredCapabilities.ipad()
-    capa.setCapability("tunnel-identifier", tunnel_id)
-
-    driver = {
-      new RemoteWebDriver(baseUrl,capa)
-    }
-  }
-  'sauce_safari' {
-    URL baseUrl = new URL("http://${sauce_username}:${sauce_access_key}@localhost:4445/wd/hub")
-    DesiredCapabilities capa = DesiredCapabilities.safari()
-    capa.setCapability("platform", "OS X 10.10");
-    capa.setCapability("version", "8.0");
-    capa.setCapability("tunnel-identifier", tunnel_id)
-
-    driver = {
-      new RemoteWebDriver(baseUrl,capa)
-    }
-  }
   'chrome-headless' {
     driver = {
-      ChromeDriverManager.getInstance().setup()
+      WebDriverManager.chromedriver().setup()
       ChromeOptions options = new ChromeOptions()
-      options.addArguments('headless')
-      options.addArguments('disable-gpu') // https://developers.google.com/web/updates/2017/04/headless-chrome
-      DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-      capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-      new ChromeDriver(capabilities)
+      options.setHeadless(true)
+      options.addArguments('disable-gpu')
+      new ChromeDriver(options)
+    }
+  }
+  'firefox' {
+    driver = {
+      WebDriverManager.firefoxdriver().setup()
+      new FirefoxDriver()
     }
   }
 }
+
 environments.each{
   env ->
     env.waiting {
